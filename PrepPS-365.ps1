ddb5df17641168d97f07cf41ae860634aac3cdc6
Justin Hawkins <18131926+JustinH2008@users.noreply.github.com>
2020-10-29 17:59:49 -0500

    Update PrepPS-365.ps1

--- PrepPS-365.ps1
+++ PrepPS-365.ps1
@@ -1,6 +1,5 @@
 ﻿Set-ItemProperty -Path 'HKLM:\SOFTWARE\Wow6432Node\Microsoft\.NetFramework\v4.0.30319' -Name 'SchUseStrongCrypto' -Value '1' -Type DWord
 Set-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\.NetFramework\v4.0.30319' -Name 'SchUseStrongCrypto' -Value '1' -Type DWord
-Powershell-Cmdlet -Confirm:$false
 Set-ExecutionPolicy Unrestricted
 Install-Module PowershellGet -Force
 Install-Module -Name CredentialManager
